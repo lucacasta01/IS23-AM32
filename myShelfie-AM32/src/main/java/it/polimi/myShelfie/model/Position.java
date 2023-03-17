63f92b23fc316ea45a86ab15f45edb74eb3d0be2
Matteo Caspani <matteo1.caspani@mail.polimi.it>
2023-03-17 15:27:51 +0100

    created position class

---
+++ myShelfie-AM32/src/main/java/it/polimi/myShelfie/model/Position.java
@@ -0,0 +1,18 @@
+package it.polimi.myShelfie.model;
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
