e6329c174b089e8528f2d17005c12c9e592ba549
Matteo Caspani <matteo1.caspani@mail.polimi.it>
2023-03-20 15:28:51 +0100

    Created TileTest class
    created test cases for Tile class

---
+++ myShelfie-AM32/src/test/java/test/TileTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import it.polimi.myShelfie.model.Tile;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TileTest {
+    @Test
+    @DisplayName("setColor method")
+    void setColor(){
+        Tile testTile1 = new Tile("testTile", Tile.Color.BLUE);
+        testTile1.setColor(Tile.Color.GREEN);
+        assertEquals(Tile.Color.GREEN, testTile1.getColor());
+        Tile testTile2 = new Tile("testTile", Tile.Color.NULLTILE);
+        testTile2.setColor(Tile.Color.PINK);
+        assertEquals(Tile.Color.PINK, testTile2.getColor());
+
+    }
+    @Test
+    @DisplayName("getColor method")
+    void getColor(){
+        Tile testTile3 = new Tile("testTile", Tile.Color.YELLOW);
+        assertEquals(Tile.Color.YELLOW, testTile3.getColor());
+        Tile testTile4 = new Tile("testTile", Tile.Color.WHITE);
+        assertEquals(Tile.Color.WHITE, testTile4.getColor());
+        Tile testTile5 = new Tile("testTile", Tile.Color.LIGHTBLUE);
+        assertEquals(Tile.Color.LIGHTBLUE, testTile5.getColor());
+
+
+    }
+
+}
