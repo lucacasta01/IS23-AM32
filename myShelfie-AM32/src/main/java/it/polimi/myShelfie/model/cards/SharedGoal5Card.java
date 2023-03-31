b2064fc0c3aac9214e4257c8335f44f3fdaa1949
Alberto Castagnoli <albicastaalbi@gmail.com>
2023-03-31 15:05:20 +0200

    Implemented CheckSharedGoal for shared goal 5 and 6

--- myShelfie-AM32/src/main/java/it/polimi/myShelfie/model/cards/SharedGoal5Card.java
+++ myShelfie-AM32/src/main/java/it/polimi/myShelfie/model/cards/SharedGoal5Card.java
@@ -1,6 +1,8 @@
 package it.polimi.myShelfie.model.cards;
 
 import it.polimi.myShelfie.model.Player;
+import it.polimi.myShelfie.model.Tile;
+import it.polimi.myShelfie.utilities.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +21,66 @@ public class SharedGoal5Card extends SharedGoalCard implements CheckSharedGoal {
      * @return Check result
      */
     public boolean checkPattern(Player p){
-        //does some stuff
-        return true;
+       if(!isAchieved(p)){
+           int achievedCols = 0;
+           int numberOfColors = 0;
+           boolean  g = false, b=false, lb=false, w=false, y=false, pi=false;
+            for(int col = 0; col< Constants.SHELFCOLUMN; col++){
+                for(int row=0; row<Constants.SHELFROW; row++){
+                    switch (p.getMyShelf().getTileMartrix()[row][col].getColor()){
+                        case BLUE:
+                            b=true;
+                            break;
+                        case GREEN:
+                            g=true;
+                            break;
+                        case LIGHTBLUE:
+                            lb=true;
+                            break;
+                        case YELLOW:
+                            y=true;
+                            break;
+                        case PINK:
+                            pi=true;
+                            break;
+                        case WHITE:
+                            w=true;
+                            break;
+                    }
+                }
+                if(b=true){
+                    numberOfColors++;
+                }
+                if(g=true){
+                    numberOfColors++;
+                }
+                if(lb=true){
+                    numberOfColors++;
+                }
+                if(y=true){
+                    numberOfColors++;
+                }
+                if(pi=true){
+                    numberOfColors++;
+                }
+                if(w=true){
+                    numberOfColors++;
+                }
+                if(numberOfColors<=3){
+                    achievedCols++;
+                }
+                b=false; g=false; lb=false; y=false; pi=false; w=false;
+
+
+            }
+            if(achievedCols>=3){
+                addPlayer(p);
+                return true;
+            }else{
+                return false;
+            }
+       }else{
+           return false;
+       }
     }
 }
