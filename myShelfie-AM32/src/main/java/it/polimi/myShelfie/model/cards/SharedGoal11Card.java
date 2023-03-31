1382a49571e14128f25d13aa174d4d51f5726e2f
Matteo Caspani <matteo1.caspani@mail.polimi.it>
2023-03-31 19:07:25 +0200

    implemented SharedGoal11 and 12 checkPattern()
    implemented SharedGoal11 and 12 checkPattern()

--- myShelfie-AM32/src/main/java/it/polimi/myShelfie/model/cards/SharedGoal11Card.java
+++ myShelfie-AM32/src/main/java/it/polimi/myShelfie/model/cards/SharedGoal11Card.java
@@ -1,6 +1,9 @@
 package it.polimi.myShelfie.model.cards;
 
 import it.polimi.myShelfie.model.Player;
+import it.polimi.myShelfie.model.Shelf;
+import it.polimi.myShelfie.model.Tile;
+import it.polimi.myShelfie.utilities.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +15,59 @@ public class SharedGoal11Card extends SharedGoalCard implements CheckSharedGoal 
         super(imgPath);
     }
 
+
     /**
      * Returns true if the player passed by parameter has achieved the shared goal
      *
      * @return Check result
      */
     public boolean checkPattern(Player p){
-        //does some stuff
-        return true;
+        if(isAchieved(p)){return false;}
+        Shelf playerShelf = p.getMyShelf();
+        Tile[][] matrix = playerShelf.getTileMartrix();
+        int countEquals = 0;
+        final int controlli = 4;
+
+        for(int i = 0; i < Constants.SHELFCOLUMN - 2; i++){
+            if(matrix[i][i].getColor().equals(matrix[i+1][i+1].getColor())){
+            countEquals++;
+            }
+        }
+        if(countEquals == controlli){return true;}
+
+        for(int i = 0; i < Constants.SHELFCOLUMN - 2; i++) {
+            if (i == 0) {
+                countEquals = 0;
+            }
+            if (matrix[i + 1][i].getColor().equals(matrix[i + 2][i + 1].getColor())){
+
+            countEquals++;
+            }
+        }
+        if(countEquals == controlli){return true;}
+
+        int j=0;
+        for(int i = Constants.SHELFCOLUMN - 1; i > 0; i--){
+            if (i == Constants.SHELFCOLUMN - 1) {
+                countEquals = 0;
+            }
+            if (matrix[i][j].getColor().equals(matrix[i-1][j+1].getColor())){
+            countEquals++;
+            }
+            j++;
+        }if(countEquals == controlli){return true;}
+
+        int k=0;
+        for(int i = Constants.SHELFCOLUMN ; i > 1; i--){
+            if (i == Constants.SHELFCOLUMN) {
+                countEquals = 0;
+            }
+            if (matrix[i][k].getColor().equals(matrix[i-1][k+1].getColor())){
+                countEquals++;
+            }
+            k++;
+        }if(countEquals == controlli){return true;}
+
+        return false;
     }
 }
--- myShelfie-AM32/src/main/java/it/polimi/myShelfie/model/cards/SharedGoal12Card.java
+++ myShelfie-AM32/src/main/java/it/polimi/myShelfie/model/cards/SharedGoal12Card.java
@@ -1,6 +1,9 @@
 package it.polimi.myShelfie.model.cards;
 
 import it.polimi.myShelfie.model.Player;
+import it.polimi.myShelfie.model.Shelf;
+import it.polimi.myShelfie.model.Tile;
+import it.polimi.myShelfie.utilities.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +14,40 @@ public class SharedGoal12Card extends SharedGoalCard implements CheckSharedGoal 
     public SharedGoal12Card(String imgPath) {
         super(imgPath);
     }
+
     /**
      * Returns true if the player passed by parameter has achieved the shared goal
      *
      * @return Check result
      */
     public boolean checkPattern(Player p){
-        //does some stuff
-        return true;
+        if(isAchieved(p)){return false;}
+
+        Shelf playerShelf = p.getMyShelf();
+        Tile[][] matrix = playerShelf.getTileMartrix();
+        int columnCount[] = new int[]{0,0,0,0,0};
+
+        for(int i = 0; i< Constants.SHELFROW - 1; i++) {
+            for (int j = 0; j < Constants.SHELFCOLUMN - 1; j++) {
+                if(!matrix[i][j].getColor().equals(Tile.Color.NULLTILE));
+                columnCount[j] ++;
+            }
+        }
+        int i=0;
+        if(columnCount[i] > columnCount[i+1]) {
+            for(int j = 0; j< Constants.SHELFROW - 2; i++){
+                if (columnCount[j] != columnCount[j + 1] + 1) {return false;}
+            }
+            return true;
+        }
+        else if(columnCount[i] < columnCount[i+1]){
+            for(int j = 0; j< Constants.SHELFROW - 2; i++) {
+                if (columnCount[i] != columnCount[i + 1] - 1) {return false;}
+            }
+            return true;
+        }
+        else if(columnCount[i] == columnCount[i+1]){return false;}
+
+        return false;
     }
 }
