1c195f4fb40011c27e8791a623a05efe1324d33b
Alberto Castagnoli <albicastaalbi@gmail.com>
2023-05-08 18:50:51 +0200

    java fx

--- src/main/java/it/polimi/myShelfie/application/GUIClient.java
+++ src/main/java/it/polimi/myShelfie/application/GUIClient.java
@@ -1,21 +1,20 @@
 package it.polimi.myShelfie.application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class GUIClient extends Application {
-
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("src/resources/Untitled.fxml"));
+       stage.setTitle("My Shelfie");
+       stage.setScene(new Scene(root));
+       stage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/it/polimi/myShelfie/application/controller/GUIController.java
@@ -0,0 +1,10 @@
+package it.polimi.myShelfie.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class GUIController {
+
+    public void login(ActionEvent actionEvent) {
+
+    }
+}
